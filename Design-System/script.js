88b6c8e5f47a4a7f30c5da07cf4f5eddb8b5202a
Traxion <p.robin.van.esch@hotmail.com>
2022-12-07 20:54:37 +0100

    Menu closing functionality implemented

--- Design-System/script.js
+++ Design-System/script.js
@@ -1,7 +1,26 @@
 // Fetch all classes named "dynamic"
 const dynamics = document.querySelectorAll('.dynamic');
+
 // Fetch the "mobile-menu" element
 const menuButton = document.getElementsByClassName("mobile-menu")[0];
+
+
+// Add class named "active" to all elements in dynamics
+function openMenu() {
+  for (const dynamic of dynamics) {
+    dynamic.classList.add('active');
+  }
+}
+
+// remove class named "active" from all elements in dynamics
+function closeMenu() {
+ 
+    for (const dynamic of dynamics) {
+      dynamic.classList.remove('active');
+    
+  }
+
+}
 
 // Find current page and add a class named active-link" to it
 function highlightCurrent() {
@@ -21,14 +40,25 @@ document.onreadystatechange = () => {
   }
 }
 
-//Call open or close function based on the current width of the screen
+// Call open or close function based on the current width of the screen
 window.onresize = function() {
   if (window.innerWidth >= 760) {
     openMenu();
+   
     } else {
     closeMenu();
+   
     };
 }
+// Listen for clicks and set the variable ignore to the navigation element
+document.addEventListener('click', function(event) {
+  const ignore = document.getElementsByClassName("navigation")[0];
+  
+  // Check if the window size is below 760px and check if the click happened outside of the navigation element and outside of the button element, if true call closeMenu()
+  if (window.innerWidth <= 759 && !ignore.contains(event.target) && !menuButton.contains(event.target) && dynamics[0].classList.contains('active')) {
+closeMenu();
+  }
+});
 
 // Add a class named "active" to all elements in dynamics
 menuButton.addEventListener("click", function() {
@@ -36,17 +66,3 @@ for (const dynamic of dynamics) {
   dynamic.classList.add('active');
 }
 });
-
-//add class named "active" to all elements in dynamics
-function openMenu() {
-  for (const dynamic of dynamics) {
-    dynamic.classList.add('active');
-  }
-}
-
-//remove class named "active" from all elements in dynamics
-function closeMenu() {
-  for (const dynamic of dynamics) {
-    dynamic.classList.remove('active');
-  }
-}
