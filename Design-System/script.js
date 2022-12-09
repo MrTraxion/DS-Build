972d3307499658fd97bb3d79b62ab9da824dcbe6
MrTraxion <robin.van.esch@Hotmail.com>
2022-12-09 10:32:22 +0100

    Working on Implementing Menu Dropdown
    Needs a rework

--- Design-System/script.js
+++ Design-System/script.js
@@ -1,6 +1,6 @@
 // Fetch all classes named "dynamic"
 const dynamics = document.querySelectorAll('.dynamic');
-
+const subLinks = document.getElementsByClassName('sub-links')[0];
 // Fetch the "mobile-menu" element
 const menuButton = document.getElementsByClassName("mobile-menu")[0];
 
@@ -14,21 +14,23 @@ function openMenu() {
 
 // remove class named "active" from all elements in dynamics
 function closeMenu() {
- 
     for (const dynamic of dynamics) {
-      dynamic.classList.remove('active');
-    
+      dynamic.classList.remove('active'); 
   }
-
 }
 
 // Find current page and add a class named active-link" to it
 function highlightCurrent() {
   const currentPage = document.URL;
-  const links = document.getElementsByTagName('a');
+  const links = document.getElementsByClassName('link');
+
   for (let link of links) {
-    if (link.href == currentPage) {
+    if (link.firstChild.href == currentPage) {
       link.classList.add("active-link");
+      if (link.classList.contains("dropdown")) {
+      subLinks.classList.add("visible");
+      }console.log(subLinks)
+      
     }
   }
 }
@@ -44,12 +46,11 @@ document.onreadystatechange = () => {
 window.onresize = function() {
   if (window.innerWidth >= 760) {
     openMenu();
-   
     } else {
-    closeMenu();
-   
+    closeMenu();   
     };
 }
+
 // Listen for clicks and set the variable ignore to the navigation element
 document.addEventListener('click', function(event) {
   const ignore = document.getElementsByClassName("navigation")[0];
